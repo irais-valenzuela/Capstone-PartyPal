14d946403a94ec26d95c8dd27b58e7402c1f80fb
irais-valenzuela <iraisvq8@gmail.com>
2023-02-08 17:58:11 -0800

    recieveing musician data from yelp api!

--- server/apiRoutes/musicians.js
+++ server/apiRoutes/musicians.js
@@ -1,16 +1,63 @@
-const musiciansRouter = require('express').Router() 
+const musiciansRouter = require("express").Router();
+const axios = require("axios")
+
+const TOKEN = process.env.YELP_TOKEN;
+
+// returns our query to query yelp api
+const userSearch = (userSearchInput) => {
+ const {service, categories, location, price} = userSearchInput
+  return `{
+    search(term: "${service}", location: "${location}", categories: "${categories}", price: "${price}", limit: 50) {
+        total
+        business {
+          id
+          name
+          is_claimed
+          photos
+          location {
+            address1
+            city
+            postal_code
+            state
+          }
+          price
+          rating
+        }
+    }
+  }`;
+};
+
+const getMusicians = async (userSearchInput) => {
+  const options = {
+    method: "POST",
+    url: "https://api.yelp.com/v3/graphql",
+    headers: {
+      "content-type": "application/graphql",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    data: userSearch(userSearchInput),
+  };
+  return axios
+    .request(options)
+    .then(function (response) {
+      const res = response.data;
+      return res;
+    })
+
+    .catch(function (error) {
+      console.error(error);
+    });
+};
 
 
-
-musiciansRouter.get('/', (req, res, next) => {
+// need a post bcs we need to send info in req.body
+musiciansRouter.post("/", async (req, res, next) => {
   try {
-    res.send('Hi in musician route').status(200)
-  } catch(error) {
-      next(error)
+    const data = await getMusicians(req.body)
+    res.send(data).status(200);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-
-
-
-module.exports = musiciansRouter
+module.exports = musiciansRouter;
