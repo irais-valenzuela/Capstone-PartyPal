23fe619628c00d9d1eb6720c3f09e0db18899e9b
irais-valenzuela <iraisvq8@gmail.com>
2023-02-14 18:20:22 -0800

    added some styling to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import ReviewsHome from './ReviewsHome';
-import CarouselHome from './CarouselHome';
-import FlexBox from './Styled-Components/FlexBox.styled';
+import React, { useEffect, useState } from "react";
+import ReviewsHome from "./ReviewsHome";
+import CarouselHome from "./CarouselHome";
+import FlexBox from "./Styled-Components/FlexBox.styled";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
-  window.localStorage.removeItem('price');
-  window.localStorage.removeItem('term');
+  window.localStorage.removeItem("price");
+  window.localStorage.removeItem("term");
 
   return (
-    <div id='home-div'>
+    <div id="home-div">
       <br></br>
       <CarouselHome />
       <div>
         <div>
           <FlexBox>
             <br></br>
-            <br></br>          
-              <h2 className='home-tagline'>
-                <strong>
-                  Have a party coming up? Need venue and catering
-                  recommendations in your area? Click Start Planning to find
-                  recommendations in your area.
-                </strong>
-              </h2>
+            <br></br>
+            <h2 className="home-tagline what-we-do">
+              <strong>
+                Have a party coming up? Need venue, catering, or entertainment
+                recommendations in your area? Click Start Planning to find
+                recommendations in your area.
+              </strong>
+            </h2>
             <br></br>
             <br></br>
             <h3>Reviews</h3>
